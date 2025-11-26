68c19b157d1891e287f5ea02c84daa958fbed5d1
Fy17 <rubik.sten@gmail.com>
2025-11-26 21:09:43 +0100

    Added testing metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import { Metadata } from "next";
+
+// TODO: Add images, icons, etc
+export const metadata: Metadata = {
+    title: "Tools for Minecraft",
+    description: "Tools for Minecraft has useful tools for minecraft users.",
+    themeColor: "#42fcff",
+    icons: {
+        icon: "/favicon.ico",
+    },
+    openGraph: {
+        title: "Tools for Minecraft",
+        description: "Tools for Minecraft has useful tools for minecraft users.",
+        url: "https://tools4minecraft.com",
+        type: "website",
+        siteName: "Tools4Minecraft",
+        locale: "en_US",
+    },
+    twitter: {
+        card: "summary",
+        title: "Tools for Minecraft",
+        description: "Tools for Minecraft has useful tools for minecraft users.",
+    },
+};
+
 export default function HomePage() {
     return (
         <div>This is the homepage.</div>
